bb207e392d131720cf8cffe6a3deac7cfbf0e81a
Woosung Jo <76472960+rickysbcws@users.noreply.github.com>
2021-09-22 06:16:22 +0000

    찾아주세요! 게시판 신규(기존 Listview 레이아웃 수정 및 sort 기능 수정)

--- WEB/assets/js/listview.js
+++ WEB/assets/js/listview.js
@@ -1,5 +1,30 @@
+const LANGUAGE_OPTION = {
+    "decimal" : "",
+    "emptyTable" : "데이터가 없습니다.",
+    "info" : "_START_ ~ _END_ (총 _TOTAL_ 개의 데이터가 있습니다.)",
+    "infoEmpty" : "0명",
+    "infoFiltered" : "(전체 _MAX_ 개 중 검색결과)",
+    "infoPostFix" : "",
+    "thousands" : ",",
+    "lengthMenu" : "_MENU_ 개씩 보기",
+    "loadingRecords" : "로딩중...",
+    "processing" : "처리중...",
+    "search" : "검색 : ",
+    "zeroRecords" : "검색된 데이터가 없습니다.",
+    "paginate" : {
+        "first" : "첫 페이지",
+        "last" : "마지막 페이지",
+        "next" : "다음",
+        "previous" : "이전"
+    },
+    "aria" : {
+        "sortAscending" : " :  오름차순 정렬",
+        "sortDescending" : " :  내림차순 정렬"
+    }
+};
+
 $(document).ready(function () {
-    var table = $('#listview').DataTable({
+    var table = $('#dataTableLost').DataTable({
         "ajax" : {
             url : '/WEB/assets/data.json', 
             type : 'GET', 
@@ -8,50 +33,32 @@ $(document).ready(function () {
             }
         }, 
         "order" : [[0, 'asc']], 
-        "language" : {
-            "decimal" : "",
-            "emptyTable" : "데이터가 없습니다.",
-            "info" : "_START_ ~ _END_ (총 _TOTAL_ 개의 데이터가 있습니다.)",
-            "infoEmpty" : "0명",
-            "infoFiltered" : "(전체 _MAX_ 명 중 검색결과)",
-            "infoPostFix" : "",
-            "thousands" : ",",
-            "lengthMenu" : "_MENU_ 개씩 보기",
-            "loadingRecords" : "로딩중...",
-            "processing" : "처리중...",
-            "search" : "검색 : ",
-            "zeroRecords" : "검색된 데이터가 없습니다.",
-            "paginate" : {
-                "first" : "첫 페이지",
-                "last" : "마지막 페이지",
-                "next" : "다음",
-                "previous" : "이전"
-            },
-            "aria" : {
-                "sortAscending" : " :  오름차순 정렬",
-                "sortDescending" : " :  내림차순 정렬"
-            }
-        }, 
-        "columnDefs": [{ 
+        "language" : LANGUAGE_OPTION, 
+        "columnDefs": [{
             targets : 0, 
-            className : 'dt-center'
+            className : 'dt-center', 
+            orderable : true
         }, {
             targets : 1, 
-            className : 'dt-left'
+            className : 'dt-left', 
+            className : 'dt-head-center', 
+            orderable : false
         }, {
             targets : 2, 
-            className : 'dt-center'
+            className : 'dt-center', 
+            orderable : false
         }, {
             targets : 3, 
             className : 'dt-center', 
+            orderable : true, 
             render : function(data, type) {
                 if (type == 'display') {
                     if (data == '1') {
-                        return '처리완료 <i class="fa fa-3 fa-check-circle datatable-paid-1"></i>';
+                        return "처리완료 <i style='color: #14c871;' class='fa fa-3 fa-check-circle datatable-paid-1'></i>";
                     } else if (data == '2') {
-                        return '처리중 <i class="fa fa-3 fa-exclamation-circle datatable-paid-0"></i>';
+                        return "처리중 <i style='color: #00a3d2;' class='fa fa-3 fa-exclamation-circle datatable-paid-0'></i>";
                     } else if (data == '3') {
-                        return '<i class="fa fa-3 fa-exclamation-circle datatable-paid-0"></i>';
+                        // return "<i class='fa fa-3 fa-exclamation-circle datatable-paid-0'></i>";
                     }
                 } else {
                     return data;
@@ -59,9 +66,10 @@ $(document).ready(function () {
             }
         }, {
             targets: -1,
-            className: 'dt-center',
+            className : 'dt-center', 
+            orderable : false, 
             data: null,
-            defaultContent: "<button>자세히 보기</button>"
+            defaultContent: "<button style='border-radius: 4px; background: #fff; color: #5f687b; border: 1px solid #cdd1d9; font-size: 14px; padding: 8px 20px;'>자세히 보기</button>"
         }],
         
         initComplete: function () {
@@ -87,8 +95,9 @@ $(document).ready(function () {
         
     });
 
-    $('#listview tbody').on('click', 'button', function () {
-        var data = table.row( $(this).parents('tr') ).data();
-        console.log(`${data[4]}번째 글입니다.`);
+    $('#dataTableLost tbody').on('click', 'button', function () {
+        var data = table.row( $(this).parents('tr')).data();
+        
+        // window.open(`https://${data[4]}`);
     });
 });
