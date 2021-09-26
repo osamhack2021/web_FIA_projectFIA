acccbeb1f7fbdcdde4161bded0ce2609796ae348
Woosung Jo <76472960+rickysbcws@users.noreply.github.com>
2021-09-26 14:09:32 +0000

    글 작성하기 이미지 업로드 기능 수정

--- WEB/assets/js/write.js
+++ WEB/assets/js/write.js
@@ -26,3 +26,31 @@ function getParameterByName(name) {
     results = regex.exec(location.search); 
     return results == null ? null : decodeURIComponent(results[1].replace(/\+/g, " ")); 
 }
+
+function uploadImgPreview() {
+    // @breif 업로드 파일 읽기
+    let fileList = document.getElementById( "upload" ).files;
+    // @breif 업로드 파일 읽기
+    function readAndPreview( fileList ) {
+        // @breif 이미지 확장자 검사
+        if ( /\.(jpe?g|png|gif)$/i.test( fileList.name ) ) {
+            let reader = new FileReader();
+            reader.addEventListener( "load", function() {
+                let image = new Image();
+                image.width = "264";
+                image.height = "264";
+                image.title = fileList.name;
+                image.src = this.result;
+                // @details 이미지 확장자 검사
+                document.getElementById( "thumbnailImgs" ).appendChild( image );
+            }, false );
+            // @details readAsDataURL( )을 통해 파일의 URL을 읽어온다.
+            if( fileList ) {
+                reader.readAsDataURL( fileList );
+            }
+        }
+    }
+    if( fileList ) {
+        [].forEach.call( fileList, readAndPreview );
+    }
+}
