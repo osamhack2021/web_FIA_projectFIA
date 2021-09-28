15f744d93931b539deef7a794838edfb5fa08e44
MoonJeWoong <31722737+MoonJeWoong@users.noreply.github.com>
2021-09-28 15:34:47 +0000

    IsOwnerOrReadOnly permission added

--- WEB/api_server/board/views.py
+++ WEB/api_server/board/views.py
@@ -2,10 +2,14 @@
 from .models import Post
 from .serializers import PostSerializer
 from rest_framework import viewsets
-
+from dj_rest_auth.jwt_auth import JWTCookieAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 
 # Post의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class PostViewSet(viewsets.ModelViewSet):
+    authentication_classes = [JWTCookieAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
