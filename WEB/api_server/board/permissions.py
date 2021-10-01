36d36a1bce269f8ddd6f0b0ff65e0b217ca344d6
MoonJeWoong <31722737+MoonJeWoong@users.noreply.github.com>
2021-10-01 09:52:33 +0000

    admin`s authority about all posts added

--- WEB/api_server/board/permissions.py
+++ WEB/api_server/board/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsAdminOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         # 읽기 권한 요청이 들어오면 허용
@@ -8,5 +8,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         
         # 요청자(request.user)가 객체(Blog)의 user와 동일한지 확인
-        # 추후 admin 계정 권한 추가하기
-        return obj.user == request.user
+        # 추후 admin 계정 권한 추가
+        print(request.user.email)
+        return obj.user == request.user or request.user.is_admin == True
--- WEB/api_server/board/views.py
+++ WEB/api_server/board/views.py
@@ -4,12 +4,12 @@ from .serializers import PostSerializer
 from rest_framework import viewsets
 from dj_rest_auth.jwt_auth import JWTCookieAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsAdminOwnerOrReadOnly
 
 # Post의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class PostViewSet(viewsets.ModelViewSet):
     authentication_classes = [JWTCookieAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminOwnerOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
