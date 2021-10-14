cfbedab34288fbec12ec36f9c06d26a8f3538657
Woosung Jo <76472960+rickysbcws@users.noreply.github.com>
2021-10-14 03:34:20 +0000

    거래완료! listview 추가

--- WEB/assets/js/listview.js
+++ WEB/assets/js/listview.js
@@ -29,6 +29,7 @@ const LANGUAGE_OPTION = {
 $(document).ready(function () {
     initDataTableLost();
     initDataTableFound();
+    initDataTableSuccess();
 });
 
 /**
@@ -119,6 +120,87 @@ function initDataTableLost() {
  */
 function initDataTableFound() {
     var table = $('#dataTableFound').DataTable({
+        "ajax" : {
+            url : '/WEB/assets/test-data.json', 
+            type : 'GET', 
+            error : function(xhr, status, error) {
+                // console.log(xhr);
+            }
+        }, 
+        "order" : [[0, 'asc']], 
+        "language" : LANGUAGE_OPTION, 
+        "columnDefs": [{
+            targets : 0, 
+            className : 'dt-center', 
+            orderable : true
+        }, {
+            targets : 1, 
+            className : 'dt-left', 
+            className : 'dt-head-center', 
+            orderable : false
+        }, {
+            targets : 2, 
+            className : 'dt-center', 
+            orderable : false
+        }, {
+            targets : 3, 
+            className : 'dt-center', 
+            orderable : true, 
+            render : function(data, type) {
+                if (type == 'display') {
+                    if (data == '1') {
+                        return "처리완료 <i style='color: #14c871;' class='fa fa-3 fa-check-circle datatable-paid-1'></i>";
+                    } else if (data == '2') {
+                        return "처리중 <i style='color: #00a3d2;' class='fa fa-3 fa-exclamation-circle datatable-paid-0'></i>";
+                    } else if (data == '3') {
+                        // return "<i class='fa fa-3 fa-exclamation-circle datatable-paid-0'></i>";
+                    }
+                } else {
+                    return data;
+                }
+            }
+        }, {
+            targets: -1,
+            className : 'dt-center', 
+            orderable : false, 
+            data: null,
+            defaultContent: "<button style='border-radius: 4px; background: #fff; color: #5f687b; border: 1px solid #cdd1d9; font-size: 14px; padding: 8px 20px;'>자세히 보기</button>"
+        }],
+        
+        initComplete: function () {
+            this.api().column().eq(0).each( function () {
+                var column = this.column(2);
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.header()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            });
+        }
+        
+    });
+
+    $('#dataTableFound tbody').on('click', 'button', function () {
+        var data = table.row( $(this).parents('tr')).data();
+        window.open(`post.html?postNo=${data[4]}`);
+    });
+}
+
+/**
+ * 거래완료! 게시판 init
+ */
+ function initDataTableSuccess() {
+    var table = $('#dataTableSuccess').DataTable({
         "ajax" : {
             url : '/WEB/assets/test-data.json', 
             type : 'GET', 
